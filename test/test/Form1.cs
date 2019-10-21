e5b46ae796c28adc56f22276a69dfedac2aadfb5
Lapastoure Corentin <Lapastoure@users.noreply.github.com>
2019-10-21 18:48:52 +0200

    c'est ok

---
+++ test/test/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using issou;
+using MySql.Data.MySqlClient;
+
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+
+        private static string server;
+        private static string database;
+        private static string uid;
+        private static string password;
+        private static string port;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Label3_Click(object sender, EventArgs e)
+        {
+      
+        server = textBox1.Text;
+        database = textBox4.Text;
+        uid = textBox3.Text;
+        password = textBox2.Text;
+        port = textBox5.Text;
+        }
+
+        public static MySqlConnection GetDBConnection()
+        {
+            return DBMySQLUtils.GetDBConnection(server,port,database,uid,password);
+        }
+
+     }
+}
